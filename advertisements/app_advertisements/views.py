3ec1f9a35ba0506482f1a45960d6bfec999f7964
Sergey <zhukmelkiy3@gmail.com>
2023-07-23 11:15:25 +0300

    dz_5

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -5,5 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
 def page1(request):
     return HttpResponse('Успешно это другая') 
